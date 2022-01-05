5607d9d5b546cfa9323eb35913bae8c6d8b74952
mike <pelanek.lukas@seznam.cz>
2022-01-05 14:08:45 +0100

    feat: payment token registry

---
+++ contracts/PaymentTokenRegistry.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+* @notice Payment token registry module which stores available
+* payment tokens for artion.
+*/
+contract PaymentTokenRegistry is Ownable {
+    event PaymentTokenAdded(address token);
+    event PaymentTokenRemoved(address token);
+
+    /**
+    * @notice ERC20 Address -> Bool
+    */
+    mapping(address => bool) public enabled;
+
+    /**
+    * @notice Method for adding payment token
+    * @dev Only admin
+    * @param token ERC20 token address
+    */
+    function add(address _token) external onlyOwner {
+        require(! enabled[_token], "PaymentTokenRegistry: payment token already added");
+        enabled[_token] = true;
+        emit PaymentTokenAdded(_token);
+    }
+
+    /**
+    * @notice Method for removing payment token
+    * @dev Only admin
+    * @param token ERC20 token address
+    */
+    function remove(address _token) external onlyOwner {
+        require(enabled[_token], "PaymentTokenRegistry: payment token does not exist");
+        enabled[_token] = false;
+        emit PaymentTokenRemoved(_token);
+    }
+}
